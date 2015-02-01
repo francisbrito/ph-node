97dacc679486c0fbbea70e1deb4a194be599c87b
Francis Brito <fr.br94@gmail.com>
2015-02-01 13:24:21 -0400

    Add some client#createPrintFile method tests.

--- test/index.js
+++ test/index.js
@@ -48,4 +48,12 @@ describe('Client', function () {
         it('should throw if API endpoint responds with a server error.');
         it('should throw if an unsupported status code is returned.');
     });
+
+    describe('#createPrintFile', function () {
+        it('should throw if no `fields` parameter is provided.');
+
+        it('should throw if API endpoint is not reachable.');
+        it('should throw if API endpoint responds with a server error.');
+        it('should throw if an unsupported status code is returned.');
+    });
 });
