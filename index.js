1e8cfe638cd336c9aabdccd33bd6049e6206093a
Francis Brito <fr.br94@gmail.com>
2015-02-01 15:18:28 -0400

    Make client should use default API if none is provided test pass.

--- index.js
+++ index.js
@@ -15,11 +15,13 @@
  * @param {Object} opts     Contains options to be passed to the client
  */
 var Client = function Client(apiKey, opts) {
+    opts = opts || {};
+
     if (!apiKey) {
         throw new Error('No API key provided.');
     }
 
-    this._apiUrl = '';
+    this._apiUrl = opts.apiUrl || Client.DEFAULT_API_URL; 
 };
 
 /**
